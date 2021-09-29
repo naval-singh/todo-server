15758fa365bcbc95f6d32883a447eb9301ff08f5
naval-singh <navalverma999@gmail.com>
2021-09-29 20:26:45 +0530

    change database server

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -6,12 +6,11 @@ exports.signupService = (req, res) => {
     return new Promise(async (resolve, reject) => {
 
         let is_active = false
-        const { email, password, role } = req.body
+        const { email, password, role, ...rest } = req.body
         const hash_password = await bcrypt.hash(password, 10)
-        const body = { ...req.body, hash_password, is_active }
-        const searchQuery = `select * from users where email='${email}'`
-
         if (role.toLowerCase() === 'user') { is_active = true }
+        const body = { ...rest, email, role, hash_password, is_active }
+        const searchQuery = `select * from users where email='${email}'`
 
         pool.query(searchQuery, (error, response) => {
             if (error) {
@@ -43,8 +42,13 @@ exports.loginService = (req, res) => {
             else if (response.length > 0) {
                 const { id, first_name, last_name, role, hash_password, is_active } = response[0]
                 if (await bcrypt.compare(password, hash_password)) {
-                    const token = jwt.sign({ id, first_name, last_name, email, is_active, role }, process.env.JWT_SECRET)
-                    resolve({ message: 'successfully logged in...', token })
+                    if (is_active == 1) {
+                        const token = jwt.sign({ id, first_name, last_name, email, is_active, role }, process.env.JWT_SECRET)
+                        resolve({ message: 'successfully logged in...', token })
+                    } else {
+                        reject({ message: 'Please activate your account before login' })
+                    }
+
                 } else {
                     reject({ message: 'Invalid password' })
                 }
