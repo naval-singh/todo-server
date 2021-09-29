0b6a8b3c9bc1add5e0c6e837fab20c3fc9dd237b
naval-singh <navalverma999@gmail.com>
2021-09-29 18:57:39 +0530

    update cors and nodemon

--- src/app.server.js
+++ src/app.server.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
+const cors = require('cors')
 const express = require('express')
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.use(cors())
 app.use(express.json())
 app.use('/api', require('./routes/auth.router'))
 app.use('/api/todo', require('./routes/todo.router'))
--- src/controller/todo.controller.js
+++ src/controller/todo.controller.js
@@ -1,6 +1,8 @@
 const { addNewTodoService, getAllTodoService, getTodoByIdService, updateTodoService, deleteTodoService } = require("../services/todo.service")
 
 exports.addNewTodo = (req, res) => {
+    const { title } = req.body
+    if (!title) { return res.status(400).json({ message: 'all field required' }) }
     addNewTodoService(req, res)
         .then(response => res.status(200).json({ response }))
         .catch(error => res.status(400).json({ error }))
@@ -19,6 +21,8 @@ exports.getTodoById = (req, res) => {
 }
 
 exports.updateTodo = (req, res) => {
+    const { title, isCompleted } = req.body
+    if (!title || !isCompleted) { return res.status(400).json({ message: 'all field required' }) }
     updateTodoService(req, res)
         .then(response => res.status(200).json({ response }))
         .catch(error => res.status(400).json({ error }))
